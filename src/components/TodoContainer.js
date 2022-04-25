ab72b6dcba22983c8b3b2d2c6cb275e7122134a7
Regiss Mukubiza <regiss.steven@gmail.com>
2022-04-25 13:08:27 +0200

    setup environment

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+export default class TodoContainer extends Component {
+    state = {
+        todos: [
+          {
+            id: 1,
+            title: "Setup development environment",
+            completed: true
+          },
+          {
+            id: 2,
+            title: "Develop website and add content",
+            completed: false
+          },
+          {
+            id: 3,
+            title: "Deploy to live server",
+            completed: false
+          }
+        ]
+       };
+  render() {
+    return (
+        <>
+          <ul>
+              {this.state.todos.map(todo => (
+                  <li>{todo.title}</li>
+              ))
+              }
+          </ul>
+        </>
+    )
+  }
+}
